be87664df77cd2fbbc7f49c8955cf8d58831afe1
yna donaire <ynadonaire@ynas-MacBook-Air.local>
2025-12-09 00:42:01 +0800

    gh

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -339,7 +339,7 @@ const AdminDashboard: React.FC = () => {
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === 'HPGlow@Admin!2025') {
+    if (password === 'Peptalk@Admin!2025') {
       setIsAuthenticated(true);
       localStorage.setItem('peptide_admin_auth', 'true');
       setLoginError('');
